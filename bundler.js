928e6ddc5dd020ccfb6678b4e8d6f25b11ba5237
youjie <youjie@hudongpai.com>
2018-11-05 16:58:07 +0800

    所有依赖关系数组处理

--- bundler.js
+++ bundler.js
@@ -1,15 +1,51 @@
 const fs = require("fs");
 const babylon = require("babylon");
 const traverse = require("babel-traverse").default;
+
+const path = require("path");
+
+let ID = 0;
+
 function createAssets(filename) {
   const content = fs.readFileSync(filename, "utf-8");
 
   const ast = babylon.parse(content, { sourceType: "module" });
 
+  const dependencies = [];
   traverse(ast, {
     ImportDeclaration: ({ node }) => {
-      console.log(node);
+      //所有import进来文件入一个依赖数组
+      dependencies.push(node.source.value);
     }
   });
+  const id = ID++;
+
+  return {
+    id,
+    filename,
+    dependencies
+  };
 }
-createAssets("./example/message.js");
+function createGraph(entry) {
+  const mainAsset = createAssets(entry);
+  const queue = [mainAsset];
+
+  for (let asset of queue) {
+    let dirname = path.dirname(asset.filename);
+    asset.mapping = {};
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+
+      const child = createAssets(absolutePath);
+
+      asset.mapping[relativePath] = child.id;
+
+      queue.push(child);
+    });
+  }
+  return queue;
+}
+
+const graph = createGraph("./example/entry.js");
+
+console.log(graph);
